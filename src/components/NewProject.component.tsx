6fde8a8e003031b9f247ab413f1dbe42d76f0432
Arun <youngdeveloper37@gmail.com>
2024-05-29 12:08:19 +0530

    FIX :: Add new project styles

--- src/components/NewProject.component.tsx
+++ src/components/NewProject.component.tsx
@@ -1,25 +1,34 @@
 import { FC } from "react";
+import { Input } from "./shared/Input.component";
 
 export const NewProject: FC = () => {
   return (
-    <div>
-      <menu>
-        <button>Cancel</button>
-        <button>Save</button>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+        <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 rounded-md py-2 px-6">Save</button>
       </menu>
-      <div>
-        <p>
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" placeholder="Add title"/>
-        </p>
-        <p>
-          <label htmlFor="description">Description</label>
-          <textarea id="description" placeholder="Add description"></textarea>
-        </p>
-        <p>
-          <label htmlFor="duedate">Due Date</label>
-          <input type="text" id="duedate" placeholder="Add due date" />
-        </p>
+      <div >
+        <Input
+          isTextArea={false}
+          type="text"
+          id="title"
+          label="Title"
+          placeholder="Add title"
+        />
+        <Input
+          isTextArea={true}
+          id="description"
+          label="Description"
+          placeholder="Add description"
+        />
+        <Input
+          isTextArea={false}
+          type="text"
+          id="duedate"
+          label="Due date"
+          placeholder="Add due date"
+        />
       </div>
     </div>
   );
