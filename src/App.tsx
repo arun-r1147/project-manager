c7216f3eaa8b169251dc1c9f8591e67b1ccdcc29
Arun <youngdeveloper37@gmail.com>
2024-05-29 11:20:50 +0530

    FIX :: remove default styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,5 @@ import { FC } from "react";
 
 export const App: FC = () => {
   return <>
-  <h1 className="my-8">Hello world</h1></>;
+  <h1 className="my-8 text-center text-5xl font-bold">Hello world</h1></>;
 };
