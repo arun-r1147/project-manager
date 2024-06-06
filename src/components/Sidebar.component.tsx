b7c24126faac00bcaac1bae186f9cf976dbb511b
Arun <youngdeveloper37@gmail.com>
2024-06-06 10:48:39 +0530

    FIX :: Add functionality for listing projects and styles

--- src/components/Sidebar.component.tsx
+++ src/components/Sidebar.component.tsx
@@ -3,18 +3,30 @@ import { Button } from "./shared/Button.component";
 
 interface SidebarProps {
   onClickAddNewProject: () => void;
+  projectsList: {
+    title: string;
+    description: string;
+    dueDate: string;
+    id:number
+  }[];
 }
-export const Sidebar: FC<SidebarProps> = ({ onClickAddNewProject }) => {
+export const Sidebar: FC<SidebarProps> = ({ onClickAddNewProject,projectsList }) => {
   return (
     <>
-      <aside className="w-1/3 h-full px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <aside className="w-1/3 h-full px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl overflow-y-auto">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
           Your projects
         </h2>
         <div>
           <Button onClick={onClickAddNewProject}> + Add Project</Button>
         </div>
-        <ul></ul>
+        <ul>{projectsList.map(project=>(
+           <li key={project.id}>
+           <p>Title: {project.title}</p>
+           <p>Description: {project.description}</p>
+           <p>Due Date: {project.dueDate}</p>
+         </li>
+        ))}</ul>
       </aside>
     </>
   );
