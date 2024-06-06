b82161eb001b33ec58cca9ce75787b4a08619c19
Arun <youngdeveloper37@gmail.com>
2024-06-06 10:39:10 +0530

    FIX :: Add functionality

--- src/components/NewProject.component.tsx
+++ src/components/NewProject.component.tsx
@@ -1,16 +1,55 @@
-import { FC } from "react";
-import { Input } from "./shared/Input.component";
+import { FC, useRef } from "react";
+import Input from "./shared/Input.component";
 
-export const NewProject: FC = () => {
+interface ProjectData {
+  title: string;
+  description: string;
+  dueDate: string;
+}
+interface NewProjectProps {
+  onSave: (data: ProjectData) => void;
+}
+
+export const NewProject: FC<NewProjectProps> = ({ onSave }) => {
+  const title = useRef<HTMLInputElement | HTMLTextAreaElement | null>(null);
+  const description = useRef<HTMLInputElement | HTMLTextAreaElement | null>(
+    null
+  );
+  const dueDate = useRef<HTMLInputElement | HTMLTextAreaElement | null>(null);
+
+  const onClickGetInputs = () => {
+    const enteredTitle = title.current?.value;
+    const enteredDesc = description.current?.value;
+    const enteredDate = dueDate.current?.value;
+    if (enteredTitle && enteredDate && enteredDesc) {
+      onSave({
+        title: enteredTitle,
+        description: enteredDesc,
+        dueDate: enteredDate,
+      });
+    }
+  };
+  const onResetForm = () => {};
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
-        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-        <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 rounded-md py-2 px-6">Save</button>
+        <button
+          onClick={onResetForm}
+          className="text-stone-800 hover:text-stone-950"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={onClickGetInputs}
+          className="bg-stone-800 text-stone-50 hover:bg-stone-950 rounded-md py-2 px-6"
+        >
+          Save
+        </button>
       </menu>
-      <div >
+      <div>
         <Input
           isTextArea={false}
+          ref={title}
           type="text"
           id="title"
           label="Title"
@@ -18,12 +57,14 @@ export const NewProject: FC = () => {
         />
         <Input
           isTextArea={true}
+          ref={description}
           id="description"
           label="Description"
           placeholder="Add description"
         />
         <Input
           isTextArea={false}
+          ref={dueDate}
           type="text"
           id="duedate"
           label="Due date"
