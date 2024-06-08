c6eff2ea33c533fbf765cedb4ac7cbd1dfa8b31b
Arun <youngdeveloper37@gmail.com>
2024-06-08 20:40:37 +0530

    FIX :: Update modal component and create portal

---
+++ src/components/shared/Modal.component.tsx
@@ -0,0 +1,32 @@
+import { forwardRef, ReactNode, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  children: ReactNode;
+}
+type DialogHandle = HTMLDialogElement & {
+    openModal: () => void;
+  }
+
+const Modal = forwardRef<DialogHandle, ModalProps>(
+  function Modal({ children }, ref) {
+    const dialog = useRef<HTMLDialogElement | null>(null);
+
+    useImperativeHandle(ref, () => {
+        const element = dialog.current!;
+        return {
+          openModal() {
+            element.showModal();
+          },
+          ...element,
+        };
+      }, []);
+
+    return createPortal(
+      <dialog ref={dialog as React.RefObject<HTMLDialogElement>}>{children}</dialog>,
+      document.getElementById("modal-root")!
+    );
+  }
+);
+
+export default Modal;
