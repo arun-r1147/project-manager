05c5edf44e5eab63772a7aa49f310809a2ba3802
Arun <youngdeveloper37@gmail.com>
2024-06-09 13:09:53 +0530

    FIX :: Fix type issue

--- src/components/NewProject.component.tsx
+++ src/components/NewProject.component.tsx
@@ -1,5 +1,6 @@
 import { FC, useRef } from "react";
 import Input from "./shared/Input.component";
+import Modal from "./shared/Modal.component";
 
 interface ProjectData {
   title: string;
@@ -9,8 +10,14 @@ interface ProjectData {
 interface NewProjectProps {
   onSave: (data: ProjectData) => void;
 }
+type DialogHandle = HTMLDialogElement & {
+  openModal: () => void;
+}
+
 
 export const NewProject: FC<NewProjectProps> = ({ onSave }) => {
+  const modal = useRef<DialogHandle| null>(null);
+
   const title = useRef<HTMLInputElement | HTMLTextAreaElement | null>(null);
   const description = useRef<HTMLInputElement | HTMLTextAreaElement | null>(
     null
@@ -21,56 +28,67 @@ export const NewProject: FC<NewProjectProps> = ({ onSave }) => {
     const enteredTitle = title.current?.value;
     const enteredDesc = description.current?.value;
     const enteredDate = dueDate.current?.value;
-    if (enteredTitle && enteredDate && enteredDesc) {
-      onSave({
-        title: enteredTitle,
-        description: enteredDesc,
-        dueDate: enteredDate,
-      });
+    if (
+      enteredTitle?.trim() == "" ||
+      enteredDate?.trim() == "" ||
+      enteredDesc?.trim() == ""
+    ) {
+      return;
     }
+    onSave({
+      title: enteredTitle ?? "",
+      description: enteredDesc ?? "",
+      dueDate: enteredDate ?? "",
+    });
   };
   const onResetForm = () => {};
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <button
-          onClick={onResetForm}
-          className="text-stone-800 hover:text-stone-950"
-        >
-          Cancel
-        </button>
-        <button
-          onClick={onClickGetInputs}
-          className="bg-stone-800 text-stone-50 hover:bg-stone-950 rounded-md py-2 px-6"
-        >
-          Save
-        </button>
-      </menu>
-      <div>
-        <Input
-          isTextArea={false}
-          ref={title}
-          type="text"
-          id="title"
-          label="Title"
-          placeholder="Add title"
-        />
-        <Input
-          isTextArea={true}
-          ref={description}
-          id="description"
-          label="Description"
-          placeholder="Add description"
-        />
-        <Input
-          isTextArea={false}
-          ref={dueDate}
-          type="text"
-          id="duedate"
-          label="Due date"
-          placeholder="Add due date"
-        />
+    <>
+      <Modal ref={modal}>
+        <p>error in datas</p>
+      </Modal>
+
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <button
+            onClick={onResetForm}
+            className="text-stone-800 hover:text-stone-950"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={onClickGetInputs}
+            className="bg-stone-800 text-stone-50 hover:bg-stone-950 rounded-md py-2 px-6"
+          >
+            Save
+          </button>
+        </menu>
+        <div>
+          <Input
+            isTextArea={false}
+            ref={title}
+            type="text"
+            id="title"
+            label="Title"
+            placeholder="Add title"
+          />
+          <Input
+            isTextArea={true}
+            ref={description}
+            id="description"
+            label="Description"
+            placeholder="Add description"
+          />
+          <Input
+            isTextArea={false}
+            ref={dueDate}
+            type="date"
+            id="duedate"
+            label="Due date"
+            placeholder="Add due date"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
--- src/components/shared/Modal.component.tsx
+++ src/components/shared/Modal.component.tsx
@@ -5,28 +5,33 @@ interface ModalProps {
   children: ReactNode;
 }
 type DialogHandle = HTMLDialogElement & {
-    openModal: () => void;
-  }
+  openModal: () => void;
+};
 
-const Modal = forwardRef<DialogHandle, ModalProps>(
-  function Modal({ children }, ref) {
-    const dialog = useRef<HTMLDialogElement | null>(null);
+const Modal = forwardRef<DialogHandle, ModalProps>(function Modal(
+  { children },
+  ref
+) {
+  const dialog = useRef<HTMLDialogElement | null>(null);
 
-    useImperativeHandle(ref, () => {
-        const element = dialog.current!;
-        return {
-          openModal() {
-            element.showModal();
-          },
-          ...element,
-        };
-      }, []);
+  useImperativeHandle(
+    ref,
+    () => {
+      const element = dialog.current!;
+      return {
+        openModal() {
+          element.showModal();
+        },
+        ...element,
+      };
+    },
+    []
+  );
 
-    return createPortal(
-      <dialog ref={dialog as React.RefObject<HTMLDialogElement>}>{children}</dialog>,
-      document.getElementById("modal-root")!
-    );
-  }
-);
+  return createPortal(
+    <dialog ref={dialog}>{children}</dialog>,
+    document.getElementById("modal-root")!
+  );
+});
 
 export default Modal;
